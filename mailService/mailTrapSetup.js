b5c0eb9fcecee6e4caeeaa7e4f74a0780e46ea21
AshishResolute <anshugourh21@gmail.com>
2026-03-12 11:52:02 +0530

    added nodemailer and mailtrap setup for bullMQ

---
+++ mailService/mailTrapSetup.js
@@ -0,0 +1,21 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+
+const filePath = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(filePath);
+
+dotenv.config({path:path.join(__dirname,'../dev.env')})
+
+const transport = nodemailer.createTransport({
+    host:process.env.SMTP_HOST,
+    port:process.env.SMTP_PORT,
+    auth:{
+        user:process.env.SMTP_USER,
+        pass:process.env.SMTP_PASS
+    }
+})
+
+export default transport
